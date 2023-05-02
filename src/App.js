7ca55b5efe16a1ff658cf246e4eeef62011343c1
mikejin01 <mikejin@outlook.com>
2023-05-02 13:53:48 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useRef} from 'react'
 import Table from "./Table"
+import ig_logo from './ig.png';
 
 function App() {
   const [players, setPlayers] = useState([])
@@ -113,7 +114,7 @@ function App() {
   return (
     <>
       <h2>jp狼人杀笔记 v1.0</h2>
-      <h3>by m.j. <a href='https://www.instagram.com/ezzekmusic/'>ig:ezzekmusic</a></h3>
+      <h3>by m.j. <a href='https://www.instagram.com/ezzekmusic/'><img src={ig_logo} style={{ width: '20px'}} ></img>ezzekmusic</a></h3>
       <div id="setting">
         <span>我的号码</span>
         <input ref={myNumberRef} type="number" pattern="\d*" style={{ width: '30px'}} ></input>
