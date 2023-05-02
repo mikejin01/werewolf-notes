062973256029b784eda1038c1978d13889cc1569
mikejin01 <mikejin@outlook.com>
2023-05-02 14:18:16 -0400

    first commit

--- src/Record.js
+++ src/Record.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Record(record) {
   return (
     <label>
-        轮次：{record.record.round} 记录类型!: {record.record.recordType} 
+        轮次：{record.record.round} 记录: {record.record.recordType} 
         <input type="checkbox" checked={record.deleted}/>
         <br></br>
     </label>
